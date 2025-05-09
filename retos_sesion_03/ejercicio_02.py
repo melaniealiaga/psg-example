1adfdaa67e1ec21b08afeba236dbbfb313288b24
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-09 15:21:45 -0400

    Agregar ejercicio 02

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,9 @@
+# Ejercicio 02
+print("Persona: ¡Hola, Michi!")
+print("Gato: Miau... ¡Hola humano!")
+print("Persona: ¿Tienes hambre?")
+print("Gato: Miau miau... ¡Siempre tengo hambre!")
+print("Persona: Bueno, vamos a la cocina.")
+print("Gato: ¡Miau! ¡Vamos!")
+print("Persona: Adiós, Michi. ¡Pórtate bien!")
+print("Gato: Miau... Adiós humano.")
