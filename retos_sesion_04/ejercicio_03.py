c7a75fdc20ce0d686a7900ebad6f219ff5b2c0d4
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-11 19:57:19 -0400

    Adicion a retos sesion 04

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,34 @@
+# Ejercicio 02
+print ("Mi juego favorito se llama La Feria de las Pulgas de Titirilquen – 31 Minutos")
+# Este es un juego de cartas donde se tiene como objetivo juntar todas las cartas de un tipo antes que los demás jugadores.
+# El juego incluye 90 cartas de chucherías (9 de cada tipo), 1 Campana y un reglamento.
+print ("Reglas de mi juego favorito:")
+print ("1. Para la preparación del juego, se debe incluir un tipo de cartas de chucherias por cada participante.")
+# Es decir, si van a jugar 6 personas, debes retirar 4 tipos cartas de chucherías
+print ("2. Se baraja y reparte 9 cartas a cada jugador.")
+print ("3. Los jugadores deben ordenar las cartas sin mostrarlas a los demas participantes.")
+print ("4. Una vez que todos los jugadores estan listos para realizar el intercambio se toca la campana y comiza el juego.")
+print ("5. Para intercambiar las cartas tan solo se debe ofrecerlas bocabajo a cualquier otra persona entre 1 a 4 cartas del mismo tipo.")
+print ("6. La otra persona te debe entregar bocabajo la misma cantidad de cartas.")
+print ("7. ¡Es trampa decir que estás ofreciendo")
+print ("8.  Siempre en un intercambio, tanto las cartas que ofreces, como las que recibes deben ser todas del mismo tipo.")
+print ("9. Al ser un juego sin turnos, el grupo completo buscará realizar intercambios al mismo tiempo.")
+print ("10. Una ronda en este juego podría sonar así: “¡Cambio tres, tres, tres, tres, tres!”. Mientras una persona al lado tuyo grita “¡Dos, dos, dos, dos!”.")
+print ("11. Si otra persona ofrece el mismo número que tú, hacen el intercambio de cartas.")
+# Con un poco de suerte, esas cartas serán útiles para completar el set de 9 cartas iguales que necesitas para ganar la ronda. 
+#Continúa intercambiando hasta lograrlo.
+print ("12. Cuando se tengan en la mano 9 cartas iguales, se toca rápidamente la campana.")
+print ("13. La ronda termina inmediatamente, y se anota en un papel el puntaje de las cartas de la persona ganadora.")
+print ("14. Solo la primera persona en tocar la campana gana los puntos.")
+print ("15. Cada personaje contiene un numero que indica cuantos puntos se recibe al ganar juntando las 9 cartas:")
+print ("    - Patana = 70 puntos")
+print ("    - Tío Horacio = 75 puntos")
+print ("    - Huachimingo = 65 puntos")
+print ("    - Guaripolo = 50 puntos")
+print ("    - Cachirula = 85 puntos")
+print ("    - Carlitos Lechuga = 60 puntos")
+print ("    - Cebollón = 55 puntos")
+print ("    - Tulio = 100 puntos")
+print ("    - Denis Danis = 80 puntos")
+print ("    - Dios = 90 puntos")
+print ("16. La primera persona en reunir 250 puntos será la ganadora.")
