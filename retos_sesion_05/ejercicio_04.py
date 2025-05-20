921e24a8d79232d5d5c53502be7477e83981e3f7
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-20 19:16:39 -0400

    Adición Retos sesión 05

---
+++ retos_sesion_05/ejercicio_04.py
@@ -0,0 +1,9 @@
+# Ejercicio 04
+# Lado del cubo en metros
+lado = 15
+
+# Calcular volumen
+volumen = lado ** 3
+
+# Mostrar resultado
+print(f"El volumen del cubo con lado de {lado} metros es: {volumen} m³")
