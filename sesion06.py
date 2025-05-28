a34095f87d08d778f246602a563dde856fa1709b
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-28 11:23:38 -0400

    Clase sesion 06

--- sesion06.py
+++ sesion06.py
@@ -1,9 +1,16 @@
-print("Tipos de datos booleanos")
-print (True)
-print (False)
-# Operaciones aritméticas con booleanos
-print (True + True)
-print (True * True)
-print (True * False)
-print (False + False)
-print (False * False)
+#Ejercicio 1
+print ("Ejemplo 1 - Comparación y Lógicos")
+numero = 20
+print (numero >= 0 and numero <= 100)
+
+#Ejercicio 2
+print ("Ejemplo 2 - Aritméticos y comparación")
+nota1 = 15
+nota2 = 20
+nota3 = 16
+print ((nota1 + nota2 + nota3) > 50)
+
+#Ejercicio 3
+print ("Ejemplo 3 - Aritméticos, comparación y lógicos")
+numero = 15
+print ((numero % 3 ==0) and (numero % 5 ==0) and (numero % 2 !=0))
