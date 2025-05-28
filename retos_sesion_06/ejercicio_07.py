afd2b403746021e700901eaf9b8afb8c696c89d3
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-28 12:28:24 -0400

    Adicion retos sesion 06

---
+++ retos_sesion_06/ejercicio_07.py
@@ -0,0 +1,8 @@
+# Ejercicio 07
+cuadrado = 2 **cuadrado = 2 ** 2       # 4
+suma = 2 + 2            # 4
+son_iguales = cuadrado == suma
+print(f"¿El cuadrado y la suma de 2 y 2 son iguales?: {son_iguales}")  # True 2       # 4
+suma = 2 + 2            # 4
+son_iguales = cuadrado == suma
+print(f"¿El cuadrado y la suma de 2 y 2 son iguales?: {son_iguales}")  # True
