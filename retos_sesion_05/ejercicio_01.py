db16927ffd3f86da8bfc295e1da76f965c314912
Melanie Aliaga <melaniealiaga00@gmail.com>
2025-05-28 11:00:03 -0400

    Adicion a retos sesion 05

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,12 @@
+# Ejercicio 01
+# Precio unitario
+precio_unitario = 3.75
+
+# Cantidad de productos
+cantidad = 12
+
+# Cálculo del total
+total = precio_unitario * cantidad
+
+# Mostrar resultado
+print(f"El total a pagar por {cantidad} productos es: ${total:.2f}")
---
+++ sesion06.py
@@ -0,0 +1,9 @@
+print("Tipos de datos booleanos")
+print (True)
+print (False)
+# Operaciones aritméticas con booleanos
+print (True + True)
+print (True * True)
+print (True * False)
+print (False + False)
+print (False * False)
